e65cec1761173c9e26f416ab8cf607f971b98a91
kenny doreus <ke519988@ucf.edu>
2024-06-26 18:22:33 -0400

    First time for everything right, hahaha

---
+++ meaning.py
@@ -0,0 +1,12 @@
+import sys
+
+
+name = input('Rock, paper, siccors?........\n')
+
+
+
+
+if (( name == "rock") | (name == "paper") | (name == "siccors")):
+    input('machine gun wins!')
+else:
+    sys.exit('Enter the right name')
